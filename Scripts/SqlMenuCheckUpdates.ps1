a97ac0cf48a6148b82929d1c160ec71fd1c9aaa5
RL_MSFT <20459971+rl-msft@users.noreply.github.com>
2020-03-03 21:19:54 +0400

    Update SqlMenuCheckUpdates.ps1

--- Scripts/SqlMenuCheckUpdates.ps1
+++ Scripts/SqlMenuCheckUpdates.ps1
@@ -1,4 +1,4 @@
-#edit: Feb 25 2019
+#Edit: Feb 25 2019
 #v2.0
 
 powershell -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/rl-msft/sqlerrorloghelper/master/uninstall.reg' -OutFile $env:temp\uninstall.reg"
